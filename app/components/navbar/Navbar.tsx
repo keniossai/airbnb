57b76cc8f1a18e58cf3cc3566e8d442ffec96c69
keniossai <kenossai1@gmail.com>
2023-04-10 11:18:09 +0100

    Added container with ReactNode

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+import Container from "../Container";
+
 const Navbar = () => {
     return ( 
-        <div className="">
-            Navbar
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    Anything
+                </Container>
+            </div>
         </div>
      );
 }
